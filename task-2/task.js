4488239b843933010513daa8ba7252b8942b79eb
A.kLYKOV <a.klykov@worksolutions.ru>
2022-08-17 13:39:59 +0300

    feat: solve task-2

---
+++ task-2/task.js
@@ -0,0 +1,11 @@
+function shallowEquals(obj1, obj2){
+    let isEqual = true;
+
+    Object.keys(obj1).forEach((key, index) => {
+        if(obj1[key] !== obj2[key]){
+            isEqual = false
+        }
+    })
+
+    return isEqual;
+}
