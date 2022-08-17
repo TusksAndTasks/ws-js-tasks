1b4005355dd30a3d8b73a243257a9f8de3417a56
A.kLYKOV <a.klykov@worksolutions.ru>
2022-08-17 13:41:37 +0300

    fix: delete redundant variable

--- task-2/task.js
+++ task-2/task.js
@@ -1,7 +1,7 @@
 function shallowEquals(obj1, obj2){
     let isEqual = true;
 
-    Object.keys(obj1).forEach((key, index) => {
+    Object.keys(obj1).forEach((key) => {
         if(obj1[key] !== obj2[key]){
             isEqual = false
         }
